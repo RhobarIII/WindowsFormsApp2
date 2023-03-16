67dddbffffec146f1e5a7979354f99d531058834
uczen <uczen@A26-13>
2023-03-16 14:58:56 +0100

    ProjectReady

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             this.mySqlConnection1 = new MySqlConnector.MySqlConnection();
+            this.mySqlCommand1 = new MySqlConnector.MySqlCommand();
+            this.txtBoxLogin = new System.Windows.Forms.TextBox();
+            this.txtBoxPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ButtonLogin = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mySqlConnection1
@@ -38,20 +44,82 @@
             this.mySqlConnection1.ProvidePasswordCallback = null;
             this.mySqlConnection1.RemoteCertificateValidationCallback = null;
             // 
+            // mySqlCommand1
+            // 
+            this.mySqlCommand1.CommandText = "SELECT count* FROM userdata WHERE login =@login AND password =@password";
+            this.mySqlCommand1.CommandTimeout = 30;
+            this.mySqlCommand1.Connection = this.mySqlConnection1;
+            this.mySqlCommand1.Transaction = null;
+            this.mySqlCommand1.UpdatedRowSource = System.Data.UpdateRowSource.None;
+            // 
+            // txtBoxLogin
+            // 
+            this.txtBoxLogin.Location = new System.Drawing.Point(188, 142);
+            this.txtBoxLogin.Name = "txtBoxLogin";
+            this.txtBoxLogin.Size = new System.Drawing.Size(615, 20);
+            this.txtBoxLogin.TabIndex = 0;
+            // 
+            // txtBoxPassword
+            // 
+            this.txtBoxPassword.Location = new System.Drawing.Point(188, 320);
+            this.txtBoxPassword.Name = "txtBoxPassword";
+            this.txtBoxPassword.Size = new System.Drawing.Size(615, 20);
+            this.txtBoxPassword.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(465, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(463, 262);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Hasło";
+            // 
+            // ButtonLogin
+            // 
+            this.ButtonLogin.Location = new System.Drawing.Point(380, 425);
+            this.ButtonLogin.Name = "ButtonLogin";
+            this.ButtonLogin.Size = new System.Drawing.Size(213, 23);
+            this.ButtonLogin.TabIndex = 4;
+            this.ButtonLogin.Text = "Zaloguj";
+            this.ButtonLogin.UseVisualStyleBackColor = true;
+            this.ButtonLogin.Click += new System.EventHandler(this.ButtonLogin_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(478, 299);
+            this.ClientSize = new System.Drawing.Size(1031, 517);
+            this.Controls.Add(this.ButtonLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxPassword);
+            this.Controls.Add(this.txtBoxLogin);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private MySqlConnector.MySqlConnection mySqlConnection1;
+        private MySqlConnector.MySqlCommand mySqlCommand1;
+        private System.Windows.Forms.TextBox txtBoxLogin;
+        private System.Windows.Forms.TextBox txtBoxPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ButtonLogin;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -12,9 +12,43 @@ namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+        private const string LOGIN_PARAMETER="@login";
+        private const string PASSWORD_PARAMETER="@password";
         public Form1()
         {
             InitializeComponent();
+            mySqlCommand1.Parameters.Add(LOGIN_PARAMETER, MySqlConnector.MySqlDbType.VarChar);
+            mySqlCommand1.Parameters.Add(PASSWORD_PARAMETER, MySqlConnector.MySqlDbType.VarChar);
+        }
+
+        private void ButtonLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mySqlConnection1.Open();
+
+                mySqlCommand1.Parameters[LOGIN_PARAMETER].Value = txtBoxLogin.Text;
+                mySqlCommand1.Parameters[PASSWORD_PARAMETER].Value = txtBoxPassword.Text;
+
+                int result=Convert.ToInt32(mySqlCommand1.ExecuteScalar());
+
+                if(result >0)
+                {
+                    MessageBox.Show("WSZYSCY ŚLĄZACY ŁĄCZIE SIĘ");
+                }
+                else
+                {
+                    MessageBox.Show("J***** KOMUNIZM");
+
+                }
+
+                mySqlConnection1.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw;
+            }
         }
     }
 }
