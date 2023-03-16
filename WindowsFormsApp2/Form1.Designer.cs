e6506cce7ed1d71ba3cc1b5c875adfcfeb2f818f
uczen <uczen@A26-13>
2023-03-16 14:21:34 +0100

    Dodaj pliki projektów.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mySqlConnection1 = new MySqlConnector.MySqlConnection();
+            this.SuspendLayout();
+            // 
+            // mySqlConnection1
+            // 
+            this.mySqlConnection1.ConnectionString = "server=localhost;uid=root;pwd=;database=testdb";
+            this.mySqlConnection1.ProvideClientCertificatesCallback = null;
+            this.mySqlConnection1.ProvidePasswordCallback = null;
+            this.mySqlConnection1.RemoteCertificateValidationCallback = null;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 299);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MySqlConnector.MySqlConnection mySqlConnection1;
+    }
+}
+
